f0d49c5afff6c3b49487d1b12cbe78ca648be8e9
tyler knapp <tgknapp11@gmail.com>
2019-06-17 20:28:26 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,3 +2,13 @@ def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
+
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
+def toadstool
+  puts status
+end
+
+def any_phrase(phrase)
+  put phrase
+end
